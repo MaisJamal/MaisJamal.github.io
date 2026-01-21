703dfd80413d82fc83db2e5b0c27054f8f9c079a
MaisJamal <mayssjamal@gmail.com>
2026-01-21 21:26:00 +0000

    Deploying to gh-pages from @ MaisJamal/MaisJamal.github.io@a061bdef8ea3037498d18f791460b523ff9f8da6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -92,15 +92,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-i-successfully-defended-my-doctoral-thesis-entitled-development-and-research-of-adaptive-planning-algorithms-and-methods-for-autonomous-vehicle-maneuvers",
+            },},{id: "news-i-successfully-defended-my-doctoral-thesis-entitled-development-and-research-of-adaptive-planning-algorithms-and-methods-for-autonomous-vehicle-maneuvers",
           title: 'I successfully defended my doctoral thesis entitled “Development and Research of Adaptive Planning...',
           description: "",
           section: "News",},{
