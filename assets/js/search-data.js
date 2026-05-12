bfb770673ba5fa804f63c3c1492fc8e3c3ec07f0
MaisJamal <mayssjamal@gmail.com>
2026-05-12 19:56:58 +0000

    Deploying to gh-pages from @ MaisJamal/MaisJamal.github.io@56c52986b65eb1bc6ad96065f02832dda9baaf92 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -68,7 +68,52 @@ ninja.data = [{
           section: "News",},{id: "news-تم-نشر-ورقتي-البحثية-принятие-решений-о-проведении-маневра-с-предсказанием-траекторных-потоков-для-беспилотного-транспортного-средства-maneuver-decision-making-with-trajectory-streams-prediction-for-autonomous-vehicles-في-وقائع-المؤتمر-الروسي-الثاني-والعشرين-للذكاء-الاصطناعي-rcai-2025-المنعقد-في-الفترة-من-6-إلى-10-أكتوبر-2025",
           title: 'تم نشر ورقتي البحثية “Принятие решений о проведении маневра с предсказанием траекторных потоков...',
           description: "",
-          section: "News",},{id: "projects-autonomous-car-deployment",
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/7_project/";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/8_project/";
+            },},{id: "projects-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/9_project/";
+            },},{id: "projects-autonomous-car-deployment",
           title: 'Autonomous Car Deployment',
           description: "Full-stack behavior planning and real-world maneuver execution on a Kia Soul platform — from simulation to road testing.",
           section: "Projects",handler: () => {
